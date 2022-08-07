2fba3dd357a00a4f2e56c365bfca77ee15f859d8
Simran Nuthalapati Yesukumar <48234600+simranyesukumar@users.noreply.github.com>
2022-08-07 17:46:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask,render_template,request,redirect,url_for,flash
+#Importing the libraries
 import sqlite3 as sql
+from flask import Flask,render_template,request,redirect,url_for,flash
+
 app=Flask(__name__)
 app.secret_key='admin123'
 
+#Creating a database and connecting to the database
 @app.route("/")
 @app.route("/index")
 def index():
     con=sql.connect("student.db")
     con.row_factory=sql.Row
     cur=con.cursor()
+    #Viewing the details for the student table 
     cur.execute("select * from student")
     data=cur.fetchall()
     return render_template("index.html",datas=data)
 
+#Adding the details of the student 
 @app.route("/add_user",methods=['POST','GET'])
 def add_user():
     if request.method=='POST':
@@ -28,6 +33,7 @@ def add_user():
         return redirect(url_for("index"))
     return render_template("add_user.html")
 
+#Editing the details of the student
 @app.route("/edit_user/<string:student_id>",methods=['POST','GET'])
 def edit_user(student_id):
     if request.method=='POST':
@@ -47,7 +53,8 @@ def edit_user(student_id):
     cur.execute("select * from student where student_id=?",(student_id,))
     data=cur.fetchone()
     return render_template("edit_user.html",datas=data)
-    
+
+#Deleting the details of a particular student
 @app.route("/delete_user/<string:student_id>",methods=['GET'])
 def delete_user(student_id):
     con=sql.connect("student.db")
